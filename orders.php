57ae1dd2e932b7914ed6c66aa01ed01f94f2baf2
Zuhdi <farhanzuhdiaan@gmail.com>
2024-06-04 20:08:26 +0700

    order date and items

--- orders.php
+++ orders.php
@@ -1,50 +1,29 @@
 <?php include 'templates/header.php'; ?>
 
-<h2>Transactions</h2>
+<h2>Orders</h2>
 <table>
   <thead>
     <tr>
-      <th>ID</th>
-      <th>Order ID</th>
+    <th>Order ID</th>
       <th>Transaction Token</th>
+      <th>Order Date</th>
+      <th>Items</th>
+      <th>Total Price</th>
     </tr>
   </thead>
   <tbody>
     <?php
-    $json = file_get_contents('http://localhost:3000/api/transaction');
-    $transactions = json_decode($json, true);
+    $json = file_get_contents('http://localhost:3000/api/order');
+    $orders = json_decode($json, true);
 
-    foreach ($transactions as $transaction) {
+    foreach ($orders as $order) {
       echo "<tr>
-              <td>{$transaction['id']}</td>
-              <td>{$transaction['order_id']}</td>
-              <td>{$transaction['transaction_token']}</td>
-            </tr>";
-    }
-    ?>
-  </tbody>
-</table>
-
-<h2>Order Details</h2>
-<table>
-  <thead>
-    <tr>
-      <th>Order ID</th>
-      <th>Item Name</th>
-      <th>Quantity</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
-    $json = file_get_contents('http://localhost:3000/api/order_details');
-    $orderDetails = json_decode($json, true);
-
-    foreach ($orderDetails as $detail) {
-      echo "<tr>
-              <td>{$detail['order_id']}</td>
-              <td>{$detail['name']}</td>
-              <td>{$detail['quantity']}</td>
-            </tr>";
+      <td>{$order['order_id']}</td>
+      <td>{$order['transaction_token']}</td>
+      <td>{$order['order_date']}</td>
+      <td>{$order['items']}</td>
+      <td>{$order['total_price']}</td>
+    </tr>";
     }
     ?>
   </tbody>
